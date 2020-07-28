16438acebfa8d6471c174d9bd997b29c02e47494
chegreyev <chegreyev@gmail.com>
2020-07-28 23:23:24 +0500

    Updates

---
+++ precache-manifest.c5bdd88eeda67aa79254b0803ed781c4.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4276f54739eec1dac28e6b597f6bd58e",
+    "url": "/fatman/index.html"
+  },
+  {
+    "revision": "d24245232622714343f0",
+    "url": "/fatman/static/css/main.411067f8.chunk.css"
+  },
+  {
+    "revision": "727b3da0aa9efcd652d5",
+    "url": "/fatman/static/js/2.bded1eb1.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/fatman/static/js/2.bded1eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d24245232622714343f0",
+    "url": "/fatman/static/js/main.40f12958.chunk.js"
+  },
+  {
+    "revision": "0578576f375dab57a446",
+    "url": "/fatman/static/js/runtime-main.1ed8ba42.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/fatman/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/fatman/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/fatman/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/fatman/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/fatman/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/fatman/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/fatman/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/fatman/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/fatman/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/fatman/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
