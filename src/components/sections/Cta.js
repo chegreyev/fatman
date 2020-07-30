23a9793d05a826cdae80cefe02814a857b2b71cd
chegreyev <chegreyev@gmail.com>
2020-07-31 01:11:39 +0500

    Fix: formatted Cta

--- src/components/sections/Cta.js
+++ src/components/sections/Cta.js
@@ -1,125 +1,157 @@
-import React, {useState} from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import { SectionProps } from '../../utils/SectionProps';
-import Input from '../elements/Input';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import { SectionProps } from "../../utils/SectionProps";
+import Input from "../elements/Input";
 
 // Button
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-  button: {
-    margin: theme.spacing(1),
-  },
-}))
-
+	button: {
+		margin: theme.spacing(1),
+	},
+}));
 
 const propTypes = {
-  ...SectionProps.types,
-  split: PropTypes.bool
-}
+	...SectionProps.types,
+	split: PropTypes.bool,
+};
 
 const defaultProps = {
-  ...SectionProps.defaults,
-  split: false
-}
+	...SectionProps.defaults,
+	split: false,
+};
 
 const Cta = ({
-  className,
-  topOuterDivider,
-  bottomOuterDivider,
-  topDivider,
-  bottomDivider,
-  hasBgColor,
-  invertColor,
-  split,
-  ...props
+	className,
+	topOuterDivider,
+	bottomOuterDivider,
+	topDivider,
+	bottomDivider,
+	hasBgColor,
+	invertColor,
+	split,
+	...props
 }) => {
+	const classes = useStyles();
 
-  const classes = useStyles()
+	const outerClasses = classNames(
+		"cta section center-content-mobile reveal-from-bottom",
+		topOuterDivider && "has-top-divider",
+		bottomOuterDivider && "has-bottom-divider",
+		hasBgColor && "has-bg-color",
+		invertColor && "invert-color",
+		className
+	);
 
-  const outerClasses = classNames(
-    'cta section center-content-mobile reveal-from-bottom',
-    topOuterDivider && 'has-top-divider',
-    bottomOuterDivider && 'has-bottom-divider',
-    hasBgColor && 'has-bg-color',
-    invertColor && 'invert-color',
-    className
-  );
+	const innerClasses = classNames(
+		"cta-inner section-inner",
+		topDivider && "has-top-divider",
+		bottomDivider && "has-bottom-divider",
+		split && "cta-split"
+	);
 
-  const innerClasses = classNames(
-    'cta-inner section-inner',
-    topDivider && 'has-top-divider',
-    bottomDivider && 'has-bottom-divider',
-    split && 'cta-split'
-  );  
+	let [isSubscibed, changeSubscibeStatus] = useState(false);
 
-  let [isSubscibed , changeSubscibeStatus] = useState(false)
+	const Subscribe = () => {
+		changeSubscibeStatus(true);
+		alert("Ваши данные отправлены!");
+	};
 
-  const Subscribe = () => {
-    changeSubscibeStatus(true)
-    alert("Ваши данные отправлены!")
-  }
+	return (
+		<section {...props} className={outerClasses}>
+		<div className="container">
+			<div className={innerClasses}>
+			{!isSubscibed ? (
+				<>
+				<div className="cta-slogan">
+					<h3 className="m-0">Записаться на пробный урок</h3>
+				</div>
+				<div className="cta-action">
+					<Input
+						id="newsletter"
+						type="email"
+						label="Subscribe"
+						labelHidden
+						hasIcon="left"
+						placeholder="e-mail"
+					>
+					<svg
+						width="16"
+						height="12"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z"
+							fill="#376DF9"
+						/>
+					</svg>
+					</Input>
 
-  return (
-    <section
-      {...props}
-      className={outerClasses}
-    >
-      <div className="container">
-        <div
-          className={innerClasses}
-        >
-          { !isSubscibed ?
-          <>
-              <div className="cta-slogan">
-                <h3 className="m-0">
-                  Записаться на пробный урок
-                </h3>
-              </div>
-              <div className="cta-action">
-                <Input id="newsletter" type="email" label="Subscribe" labelHidden hasIcon="left" placeholder="e-mail">
-                  <svg width="16" height="12" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z" fill="#376DF9" />
-                  </svg>
-                </Input>
+					<Input
+						id="newsletter"
+						type="text"
+						label=""
+						labelHidden
+						hasIcon="left"
+						placeholder="ФИО"
+					>
+					<svg
+						width="16"
+						height="12"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z"
+							fill="#376DF9"
+						/>
+					</svg>
+					</Input>
 
-                <Input id="newsletter" type="text" label="" labelHidden hasIcon="left" placeholder="ФИО">
-                  <svg width="16" height="12" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z" fill="#376DF9" />
-                  </svg>
-                </Input>
+					<Input
+						id="newsletter"
+						type="text"
+						label=""
+						labelHidden
+						hasIcon="left"
+						placeholder="Класс"
+					>
+					<svg
+						width="16"
+						height="12"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z"
+							fill="#376DF9"
+						/>
+					</svg>
+					</Input>
 
-                <Input id="newsletter" type="text" label="" labelHidden hasIcon="left" placeholder="Класс">
-                  <svg width="16" height="12" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z" fill="#376DF9" />
-                  </svg>
-                </Input>
-
-                <Button
-                  variant="contained"
-                  size="small"
-                  color="primary"
-                  className={classes.button}
-                  disabled={false}
-                  onClick={Subscribe}
-                >
-                  Записаться
-                </Button>
-            </div>
-          </>
-          : <div className="cta-slogan">
-              <h3 className="m-0">
-                Вы успешно записались на пробный урок !
-              </h3>
-            </div>
-          }
-        </div>
-      </div>
-    </section>
-  );
-}
+					<Button
+						variant="contained"
+						size="small"
+						color="primary"
+						className={classes.button}
+						disabled={false}
+						onClick={Subscribe}
+					>
+						Записаться
+					</Button>
+				</div>
+				</>
+			) : (
+				<div className="cta-slogan">
+					<h3 className="m-0">Вы успешно записались на пробный урок !</h3>
+				</div>
+			)}
+			</div>
+		</div>
+	</section>
+	);
+};
 
 Cta.propTypes = propTypes;
 Cta.defaultProps = defaultProps;
