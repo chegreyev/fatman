e9ce0f20ea799766a9aebf86958425672cdbd06e
chegreyev <chegreyev@gmail.com>
2020-07-30 15:39:39 +0500

    ADD: changing Cta content

--- src/components/sections/Cta.js
+++ src/components/sections/Cta.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { SectionProps } from '../../utils/SectionProps';
@@ -55,6 +55,13 @@ const Cta = ({
     split && 'cta-split'
   );  
 
+  let [isSubscibed , changeSubscibeStatus] = useState(false)
+
+  const Subscribe = () => {
+    changeSubscibeStatus(true)
+    alert("Ваши данные отправлены!")
+  }
+
   return (
     <section
       {...props}
@@ -64,41 +71,50 @@ const Cta = ({
         <div
           className={innerClasses}
         >
-          <div className="cta-slogan">
-            <h3 className="m-0">
-              Записаться на пробный урок
-            </h3>
-          </div>
-          <div className="cta-action">
-            <Input id="newsletter" type="email" label="Subscribe" labelHidden hasIcon="left" placeholder="e-mail">
-              <svg width="16" height="12" xmlns="http://www.w3.org/2000/svg">
-                <path d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z" fill="#376DF9" />
-              </svg>
-            </Input>
+          { !isSubscibed ?
+          <>
+              <div className="cta-slogan">
+                <h3 className="m-0">
+                  Записаться на пробный урок
+                </h3>
+              </div>
+              <div className="cta-action">
+                <Input id="newsletter" type="email" label="Subscribe" labelHidden hasIcon="left" placeholder="e-mail">
+                  <svg width="16" height="12" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z" fill="#376DF9" />
+                  </svg>
+                </Input>
 
-            <Input id="newsletter" type="text" label="" labelHidden hasIcon="left" placeholder="ФИО">
-              <svg width="16" height="12" xmlns="http://www.w3.org/2000/svg">
-                <path d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z" fill="#376DF9" />
-              </svg>
-            </Input>
+                <Input id="newsletter" type="text" label="" labelHidden hasIcon="left" placeholder="ФИО">
+                  <svg width="16" height="12" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z" fill="#376DF9" />
+                  </svg>
+                </Input>
 
-            <Input id="newsletter" type="text" label="" labelHidden hasIcon="left" placeholder="Класс">
-              <svg width="16" height="12" xmlns="http://www.w3.org/2000/svg">
-                <path d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z" fill="#376DF9" />
-              </svg>
-            </Input>
+                <Input id="newsletter" type="text" label="" labelHidden hasIcon="left" placeholder="Класс">
+                  <svg width="16" height="12" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z" fill="#376DF9" />
+                  </svg>
+                </Input>
 
-            <Button
-              variant="contained"
-              size="small"
-              color="primary"
-              className={classes.button}
-              disabled={false}
-              onClick={() => { alert('Ваша заявка на пробный урок отправлена !') }}
-            >
-              Записаться
-            </Button>
-          </div>
+                <Button
+                  variant="contained"
+                  size="small"
+                  color="primary"
+                  className={classes.button}
+                  disabled={false}
+                  onClick={Subscribe}
+                >
+                  Записаться
+                </Button>
+            </div>
+          </>
+          : <div className="cta-slogan">
+              <h3 className="m-0">
+                Вы успешно записались на пробный урок !
+              </h3>
+            </div>
+          }
         </div>
       </div>
     </section>
