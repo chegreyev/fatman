dfa91340df95f04676d8521c3695d1494e6fe5db
chegreyev <chegreyev@gmail.com>
2020-08-02 02:34:13 +0500

    ADD: react-hook-form first input

--- src/components/sections/Cta.js
+++ src/components/sections/Cta.js
@@ -4,15 +4,9 @@ import classNames from "classnames";
 import { SectionProps } from "../../utils/SectionProps";
 import Input from "../elements/Input";
 
-// Button
-import Button from "@material-ui/core/Button";
-import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) => ({
-	button: {
-		margin: theme.spacing(1),
-	},
-}));
+// React Hook form
+import { useForm } from "react-hook-form"
 
 const propTypes = {
 	...SectionProps.types,
@@ -35,7 +29,6 @@ const Cta = ({
 	split,
 	...props
 }) => {
-	const classes = useStyles();
 
 	const outerClasses = classNames(
 		"cta section center-content-mobile reveal-from-bottom",
@@ -54,10 +47,12 @@ const Cta = ({
 	);
 
 	let [isSubscibed, changeSubscibeStatus] = useState(false);
+	const { handleSubmit, register, errors } = useForm()
 
-	const Subscribe = () => {
+	const Subscribe = values => {
 		changeSubscibeStatus(true);
 		alert("Ваши данные отправлены!");
+		console.log(values);
 	};
 
 	return (
@@ -70,76 +65,86 @@ const Cta = ({
 					<h3 className="m-0">Записаться на пробный урок</h3>
 				</div>
 				<div className="cta-action">
-					<Input
-						id="newsletter"
-						type="email"
-						label="Subscribe"
-						labelHidden
-						hasIcon="left"
-						placeholder="e-mail"
-					>
-					<svg
-						width="16"
-						height="12"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z"
-							fill="#376DF9"
-						/>
-					</svg>
-					</Input>
+					<form onSubmit={handleSubmit(Subscribe)}>
+						<div className="has-icon-left">
+							<input 
+								className="form-input" 
+								name="email" 
+								placeholder="email"
+								ref={register({
+									required: "Required",
+									pattern: {
+										value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+										message: "Invalid email address"
+									}
+								})}	
+							/>
+							<svg
+								width="16"
+								height="12"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z"
+									fill="#376DF9"
+								/>
+							</svg>
+						</div>
+						{errors.email && errors.email.message}
 
-					<Input
-						id="newsletter"
-						type="text"
-						label=""
-						labelHidden
-						hasIcon="left"
-						placeholder="ФИО"
-					>
-					<svg
-						width="16"
-						height="12"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z"
-							fill="#376DF9"
-						/>
-					</svg>
-					</Input>
+						<Input
+							id="newsletter"
+							type="text"
+							label=""
+							labelHidden
+							hasIcon="left"
+							placeholder="ФИО"
+						>
+						<svg
+							width="16"
+							height="12"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z"
+								fill="#376DF9"
+							/>
+						</svg>
+						</Input>
 
-					<Input
-						id="newsletter"
-						type="text"
-						label=""
-						labelHidden
-						hasIcon="left"
-						placeholder="Класс"
-					>
-					<svg
-						width="16"
-						height="12"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z"
-							fill="#376DF9"
-						/>
-					</svg>
-					</Input>
+						<Input
+							id="newsletter"
+							type="text"
+							label=""
+							labelHidden
+							hasIcon="left"
+							placeholder="Класс"
+						>
+						<svg
+							width="16"
+							height="12"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z"
+								fill="#376DF9"
+							/>
+						</svg>
+						</Input>
 
-					<Button
-						variant="contained"
-						size="small"
-						color="primary"
-						className={classes.button}
-						disabled={false}
-						onClick={Subscribe}
-					>
-						Записаться
-					</Button>
+						{/* <Button
+							variant="contained"
+							size="small"
+							color="primary"
+							className={classes.button}
+							disabled={false}
+							onClick={Subscribe}
+							type="submit"
+						>
+							Записаться
+						</Button> */}
+						<input type="submit"/>
+					</form>
 				</div>
 				</>
 			) : (
