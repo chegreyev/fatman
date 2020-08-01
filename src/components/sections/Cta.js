28c6a550c7ce5f5829ed29394c1dd1aee74c2a92
chegreyev <chegreyev@gmail.com>
2020-08-02 03:32:49 +0500

    ADD: functionality for inputs and error-message styles

--- src/components/sections/Cta.js
+++ src/components/sections/Cta.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 import { SectionProps } from "../../utils/SectionProps";
-import Input from "../elements/Input";
 
 
 // React Hook form
@@ -51,9 +50,11 @@ const Cta = ({
 
 	const Subscribe = values => {
 		changeSubscibeStatus(true);
-		alert("Ваши данные отправлены!");
 		console.log(values);
 	};
+
+	const gradeValidation = (grade) => grade >= 1 && grade <= 11;
+	
 
 	return (
 		<section {...props} className={outerClasses}>
@@ -66,13 +67,16 @@ const Cta = ({
 				</div>
 				<div className="cta-action">
 					<form onSubmit={handleSubmit(Subscribe)}>
+						{
+							errors.email && <p className="error-message">{errors.email.message}</p>
+						}
 						<div className="has-icon-left">
 							<input 
 								className="form-input" 
 								name="email" 
 								placeholder="email"
 								ref={register({
-									required: "Required",
+									required: "Почта обязательна",
 									pattern: {
 										value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
 										message: "Invalid email address"
@@ -90,59 +94,55 @@ const Cta = ({
 								/>
 							</svg>
 						</div>
-						{errors.email && errors.email.message}
 
-						<Input
-							id="newsletter"
-							type="text"
-							label=""
-							labelHidden
-							hasIcon="left"
-							placeholder="ФИО"
-						>
-						<svg
-							width="16"
-							height="12"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z"
-								fill="#376DF9"
+						{
+							errors.name && <p className="error-message">{errors.name.message}</p>
+						}
+						<div className="has-icon-left">
+							<input 
+								className="form-input" 
+								name="name" 
+								placeholder="ФИО"
+								ref={register({
+									required: "Введите ваше ФИО"
+								})}	
 							/>
-						</svg>
-						</Input>
+							<svg
+								width="16"
+								height="12"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z"
+									fill="#376DF9"
+								/>
+							</svg>
+						</div>
 
-						<Input
-							id="newsletter"
-							type="text"
-							label=""
-							labelHidden
-							hasIcon="left"
-							placeholder="Класс"
-						>
-						<svg
-							width="16"
-							height="12"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z"
-								fill="#376DF9"
+						{
+							errors.grade && <p className="error-message">{errors.grade.message}</p> && <p className="error-message">Неправильный класс</p>
+						}
+						<div className="has-icon-left">
+							<input 
+								className="form-input" 
+								name="grade" 
+								placeholder="Класс ребенка"
+								ref={register({
+									required: "Введите класс обучения",
+									validate: gradeValidation
+								})}	
 							/>
-						</svg>
-						</Input>
-
-						{/* <Button
-							variant="contained"
-							size="small"
-							color="primary"
-							className={classes.button}
-							disabled={false}
-							onClick={Subscribe}
-							type="submit"
-						>
-							Записаться
-						</Button> */}
+							<svg
+								width="16"
+								height="12"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z"
+									fill="#376DF9"
+								/>
+							</svg>
+						</div>
 						<input type="submit"/>
 					</form>
 				</div>
